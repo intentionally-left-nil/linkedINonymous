6ed2029188126320aaed19f36f34757979bad1f6
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-04-10 23:53:17 -0700

    Implement getNickname

---
+++ src/js/content/getNickname.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-bitwise */
+import constellations from 'constellations';
+
+const hashCode = str => str.split('').reduce((prevHash, currVal) =>
+  (((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0);
+
+const getNickname = (name) => {
+  const index = hashCode(name) % constellations.length;
+  return constellations[index].name;
+};
+
+export default getNickname;
