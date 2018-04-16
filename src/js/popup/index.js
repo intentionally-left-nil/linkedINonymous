a6bf2a83db2330d15260808ff94289c0f449b7ba
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-04-16 01:52:01 -0700

    change the logo to grayscale

--- src/js/popup/index.js
+++ src/js/popup/index.js
@@ -5,3 +5,12 @@ import App from './App';
 
 const container = document.getElementById('app-container');
 render(<App />, container);
+
+chrome.browserAction.setIcon({
+  path: {
+    16: 'graylogo-16.png',
+    32: 'graylogo-32.png',
+    48: 'graylogo-48.png',
+    128: 'graylogo-128.png',
+  },
+});
