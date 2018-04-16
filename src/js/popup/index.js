5cc22ad5c8cb39a930b8a4f1c6b265c075169201
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-04-16 01:22:51 -0700

    Move logos to a static folder

--- src/js/popup/index.js
+++ src/js/popup/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import '../../img/icon-128.png';
-import '../../img/icon-32.png';
 
 
 const container = document.getElementById('app-container');
